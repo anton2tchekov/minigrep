a1e266bca7c6f857627b51bdc1e2d763de9fe2e8
root <root@EPI-DEV-W10-NMT.gtt.fr>
2024-11-25 17:00:46 +0100

    feat: redirect error to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use minigrep::Config;
 use std::env;
 use std::error::Error;
+use std::process;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let request = Config::new(&args)?;
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    if let Err(e) = minigrep::run(request) {
-        panic!("Application error: {e}")
-    };
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 
     Ok(())
 }
